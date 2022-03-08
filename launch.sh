2d81321230a6b11f773645e951b26c61b598bbac
Xavier Lenne <thrawn.1809@gmail.com>
2022-03-08 12:41:39 +0100

    test automatisation installation

--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo apt-get update
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 docker pull franec59/arenamodel:latest
 
