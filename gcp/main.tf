9f840d623bda050477d73b25afd6e5bf4eb076c4
Xavier Lenne <thrawn.1809@gmail.com>
2022-03-08 10:08:19 +0100

    Correctiof providers

--- gcp/main.tf
+++ gcp/main.tf
@@ -4,6 +4,10 @@ terraform{
             source = "hashicorp/google"
             version = "4.9.0"
         }
+        mongodbatlas = {
+            source = "mongodb/mongodbatlas"
+            version = "1.3.0"
+        }
     }
 }
 
