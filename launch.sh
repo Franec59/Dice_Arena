131ab3b11b7511357db4388ed5414f1def274905
Xavier Lenne <thrawn.1809@gmail.com>
2022-03-08 15:58:30 +0100

    Modif script

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
