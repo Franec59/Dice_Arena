e4d9ad9eb336dc6e24cc169724460938f15d39b4
Xavier Lenne <thrawn.1809@gmail.com>
2022-03-08 15:22:30 +0100

    Remove ansible

--- launch.sh
+++ launch.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update
 
-# sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
 
-# sudo apt-get update
-
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-docker pull franec59/arenamodel:latest
-
-docker pull franec59/arenaback:latest
-
-docker pull franec59/arena:latest
-
-docker run -d -p 8080:8080 franec59/arena
-
-docker run -d -p 8000:80 franec59/arenaback
-
-docker run -d -p 8020:90 franec59/arenamodel
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
