0fb032e518841696219eee6206b6072695ea1c85
Xavier Lenne <thrawn.1809@gmail.com>
2022-03-09 10:28:53 +0100

    MAJ script

--- launch.sh
+++ launch.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo docker login
 
 cd web/backend
 
-docker-compose up -d
+sudo docker-compose up -d
 
 sudo docker pull franec59/arenamodel:latest
 
