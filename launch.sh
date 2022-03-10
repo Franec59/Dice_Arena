d868c50278cb5af379da941e1afb98e19b6da30d
Ankoburame <thrawn.1809@gmail.com>
2022-03-10 16:23:47 +0100

    MAJ launch

--- launch.sh
+++ launch.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt update -y
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo apt install docker-compose
+apt-cache policy docker-ce -y
+sudo apt install docker-ce -y
+sudo apt install docker-compose -y
 sudo docker login
 
 cd web/backend
 
 sudo docker-compose up -d
 
-sudo docker pull franec59/arenamodel:latest
+cd ../frontend
 
-sudo docker pull franec59/arena:latest
+sudo docker-compose up -d
 
-sleep 15
+cd ../model
 
-sudo docker run -d -p 8080:8080 franec59/arena
+sudo docker-compose up -d
 
-sudo docker run -d -p 8020:90 franec59/arenamodel
+# sudo docker pull franec59/arenamodel:latest
+
+# sudo docker pull franec59/arena:latest
+
+# sleep 5
+
+# sudo docker run -d -p 8080:8080 franec59/arena
+
+# sudo docker run -d -p 8020:90 franec59/arenamodel
